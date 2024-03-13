8a056048e5bb7ca70f22525d275e0ee70b7f9fdd
JhimmieC137 <toluwalope.david@gmail.com>
2024-03-13 15:17:41 -0700

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    word = ''
+    for x in str:
+        if ord(x) in range(97, 123):
+            word = word + chr(ord(x) - 32)
+        else:
+            word = word + x
+    print(word)
