2c61bc945fc364caced2019e19d3c374285cdd43
JhimmieC137 <toluwalope.david@gmail.com>
2024-03-13 14:10:47 -0700

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
     if x == 99:
-        print("%02d" % (x), end='\n')
+        print("{}".format("%02d" % x), end='\n')
     else:
-        print("%02d" % (x), end=', ')
+        print("{}".format("%02d" % x), end=', ')
