6ae7cebc07918807e662772ea630bc318d05234d
JhimmieC137 <toluwalope.david@gmail.com>
2024-03-14 06:33:26 -0700

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+        for x in range(1, len(sys.argv)):
+            print(f"{x}: {sys.argv[x]}")
