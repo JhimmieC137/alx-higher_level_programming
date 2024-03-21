53e0fabd8602c6c3dbd9a191e022e64ecf5d5178
JhimmieC137 <toluwalope.david@gmail.com>
2024-03-20 18:40:30 -0700

    12-roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    num_dict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    num_dict = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1
+    }
+    s_len = len(roman_string)
     result = 0
     x = 0
     if roman_string and type(roman_string) == str:
         while x < len(roman_string):
             if roman_string[x] == "C":
-                if (x + 1) <= (len(roman_string) - 1) and  roman_string[x + 1] == "D":
+                if (x + 1) <= (s_len - 1) and roman_string[x + 1] == "D":
                     result += 400
                     x += 1
                 else:
                     result += num_dict[roman_string[x]]
             elif roman_string[x] == "X":
-                if (x + 1) <= (len(roman_string) - 1) and  roman_string[x + 1] == "L":
+                if (x + 1) <= (s_len - 1) and roman_string[x + 1] == "L":
                     result += 40
                     x += 1
                 else:
                     result += num_dict[roman_string[x]]
-            elif (x + 1) <= (len(roman_string) - 1) and  roman_string[x] == "I":
-                if (x + 1) <= (len(roman_string) - 1) and  roman_string[x + 1] == "X":
+            elif roman_string[x] == "I":
+                if (x + 1) <= (s_len - 1) and roman_string[x + 1] == "X":
                     result += 9
                     x += 1
-                elif (x + 1) <= (len(roman_string) - 1) and roman_string[x + 1] == "V":
+                elif (x + 1) <= (s_len - 1) and roman_string[x + 1] == "V":
                     result += 4
                     x += 1
                 else:
