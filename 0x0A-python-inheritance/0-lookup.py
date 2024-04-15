0a0e74f4c602a8c915ebb059070e79e73a05eced
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-15 06:15:39 -0700

    0-lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This python script returns a list of methods within a given class"""
+
+
+def lookup(obj):
+    """The lookup function takes in a class or class
+        object and return a list of contained methods
+
+    Args: 
+        <class> obj
+    Returns:
+        <list> 
+    """
+    return list(dir(obj))
