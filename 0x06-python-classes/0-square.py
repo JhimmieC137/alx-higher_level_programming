9a521c96adc2420b00024ba4eca16915dad10581
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-15 05:50:07 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+"""This is a python module defining a class
+
+"""
+class Square:
+    """The square does nothing, has no attributes and accepts no arguments
+    Args:
+        None
+
+    Attributes:
+        None
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+### About Classes in python
