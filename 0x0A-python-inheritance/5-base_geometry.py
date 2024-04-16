385a9af137885802193cc8f4c037f533479c3738
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-16 15:07:45 -0700

    5-base_geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script contains and empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry
+        This is an empty class that takes no arguments
+        and returns nothing
+    """
+    ...
