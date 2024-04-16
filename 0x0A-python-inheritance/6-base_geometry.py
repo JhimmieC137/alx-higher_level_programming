1159b90a85f77b9a28b5ac52309bffcee523e402
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-16 15:20:14 -0700

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+
+"""This script contains class with methods"""
+
+
+class BaseGeometry(BaseGeometry):
+    """BaseGeometry
+        This is a subclass of the BaseGeometry class
+    """
+    def area(self):
+        """This mther argument other than the reference self
+            and raises and exeption
+            Args:
+               self(None)
+            Returns:
+                None
+        """
+        raise Exception("area() is not implemented")
