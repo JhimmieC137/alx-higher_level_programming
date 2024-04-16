f795a35957bfdf86a3dfb8499eb1b1c345ae6b90
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-16 15:01:29 -0700

    4-inherits_from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This python document returns a boolean on confimation of an object type"""
+
+
+def inherits_from(obj, a_class):
+    """This function checks if an object is the instance of a class
+    Args:
+        obj (any): An object
+        a_class (any): A class
+    Return:
+        boolean
+    """
+    return issubclass(type(obj), a_class)
