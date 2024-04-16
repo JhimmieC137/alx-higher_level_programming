6aad25eaf4ec8c587decdb765675f613aa174ba1
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-16 11:21:47 -0700

    2-is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This python document returns a boolean on confimation of an object type"""
+
+
+def is_same_class(obj, a_class):
+    """This function checks if an object is the instance of a class
+
+    Args:
+        obj (any): An object
+        a_class (any): A class
+
+    Return:
+        boolean
+    """
+    return type(obj) == a_class
