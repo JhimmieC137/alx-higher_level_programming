def839af1f68b2bb2cc65a2a2fd2cff5190074bd
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-17 05:48:19 -0700

    8-rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,8 +8,18 @@ class Rectangle(BaseGeometry):
         has private attributes width and height as an update
     """
     def __init__(self, width, height):
-        """Initializing the class so we can define attributes"""
-        if super().integer_validator("width", width):
-            self.__width = width
-        if super().integer_validator("height", height):
-            self.__height = height
+        """Initializing the class so we can define attributes as private"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Check if width is a valid integer"""
+        super().integer_validator("width", width)
+        return self.__width
+
+    @property
+    def height(self):
+        """Check if height is a valid integer"""
+        super().integer_validator("height", height)
+        return self.__height
