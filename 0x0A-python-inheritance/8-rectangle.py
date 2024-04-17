a2ab1868a09254425e311d62109361437a98dc2c
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-17 06:00:10 -0700

    8-rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -14,12 +14,16 @@ class Rectangle(BaseGeometry):
 
     @property
     def width(self):
-        """Check if width is a valid integer"""
+        """Check if width is a positive integer"""
+        if width < 0:
+            width *= -1
         super().integer_validator("width", width)
         return self.__width
 
     @property
     def height(self):
-        """Check if height is a valid integer"""
+        """Check if height is a positive integer"""
+        if height < 0:
+            height *= -1
         super().integer_validator("height", height)
         return self.__height
