298af10641401ecba095ff6273762c0a571d99c9
JhimmieC137 <toluwalope.david@gmail.com>
2024-04-17 01:36:53 -0700

    4-inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,4 +13,4 @@ def inherits_from(obj, a_class):
     if type(obj) == a_class:
         return False
     else:
-        return issubclass(type(obj), a_class) 
+        return issubclass(type(obj), a_class)
