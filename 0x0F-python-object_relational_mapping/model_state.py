c06d4f69c45025c4a5b2ef5d9d68847da660d676
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-26 05:25:06 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,39 +6,26 @@
     engine
 """
 
-if __name__ == "__main__":
+import sys
+from sqlalchemy import (MetaData, Table,
+                        create_engine, Column, Integer, String)
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-    import sys
-    from sqlalchemy import (MetaData, Table,
-                            create_engine, Column, Integer, String)
-    from sqlalchemy.orm import sessionmaker, declarative_base
 
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3],
-            )
+Base = declarative_base()
+
+class State(Base):
+    """
+        The state model with attributes
+    """
+    __tablename__ = "states"
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
         )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base = declarative_base()
-    Base.metadata.create_all(engine)
-    metadata = MetaData()
-
-    class State(Base):
-        """
-            The state model with attributes
-        """
-        __tablename__ = "states"
-        id = Column(
-            Integer,
-            primary_key=True,
-            autoincrement=True,
-            nullable=False
-            )
-        name = Column(
-            String(128),
-            nullable=False
-            )
+    name = Column(
+        String(128),
+        nullable=False
+        )
