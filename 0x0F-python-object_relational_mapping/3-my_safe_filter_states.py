c70e8496a5d59aedd356a5edbed4a71e18be7e5b
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-25 22:32:21 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    This script prints query results
+    filtering all entries that match given
+    argument from a database table in the
+    terminal
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY ID ASC"
+        .format(str(sys.argv[4]))
+    )
+    for name in cur.fetchall():
+        print(name)
