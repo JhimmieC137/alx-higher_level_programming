7bcbda09c82598b1febd073a06276326cb21563b
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-25 21:50:20 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    This script prints query results
+    filtering all entries that match given
+    argument from a database table in the
+    terminal
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute(
+        f"SELECT * FROM states WHERE name LIKE '{sys.argv[4]}' ORDER BY ID ASC"
+    )
+    for name in cur.fetchall():
+        print(name)
