005a9cc373d7160e9dec3da51ae6e1f359c70e7c
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-26 05:09:04 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 
-import sys
-from sqlalchemy import (MetaData, Table,
-                        create_engine, Column, Integer, String)
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-
 """
     This script creates the model
     state and binds it to the database
     engine
 """
 
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'
-    .format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3],
-        )
-    )
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
-Base.metadata.create_all(engine)
-metadata = MetaData()
+if __name__ == "__main__":
 
-class State(Base):
-    """
-        The state model with attributes
-    """
-    __tablename__ = "states"
-    id = Column(
-        Integer,
-        primary_key=True,
-        autoincrement=True,
-        nullable=False
+    import sys
+    from sqlalchemy import (MetaData, Table,
+                            create_engine, Column, Integer, String)
+    from sqlalchemy.orm import sessionmaker, declarative_base
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3],
+            )
         )
-    name = Column(
-        String(128),
-        nullable=False
-        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base = declarative_base()
+    Base.metadata.create_all(engine)
+    metadata = MetaData()
+
+    class State(Base):
+        """
+            The state model with attributes
+        """
+        __tablename__ = "states"
+        id = Column(
+            Integer,
+            primary_key=True,
+            autoincrement=True,
+            nullable=False
+            )
+        name = Column(
+            String(128),
+            nullable=False
+            )
