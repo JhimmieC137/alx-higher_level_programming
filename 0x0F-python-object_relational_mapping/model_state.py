d5d99307369ea33f5fb915fc4a7f6f556ddb94cd
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-26 04:28:29 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,36 +3,36 @@
     This script creates the model
     State
 """
+import sys
+from sqlalchemy import (create_engine, Column, Integer, String)
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import declarative_base
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'
-            .format(
-                sys.argv[1],
-                sys.argv[2],
-                sys.argv[3],
-            )
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'
+    .format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+        )
     )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base = declarative_base()
-    Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+Base.metadata.create_all(engine)
 
-    class State(Base):
-        """
-            The state model with attributes
-        """
-        __tablename__ = "states"
-        id = Column(
-            Integer,
-            primary_key=True,
-            autoincrement=True,
-            nullable=False
-            )
-        name = Column(
-                String(128),
-                nullable=False
-                )
+
+class State(Base):
+    """
+        The state model with attributes
+    """
+    __tablename__ = "states"
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+        )
+    name = Column(
+        String(128),
+        nullable=False
+        )
