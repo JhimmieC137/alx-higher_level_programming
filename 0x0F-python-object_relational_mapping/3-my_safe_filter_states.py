02d39125ae01a361c9e58bd66340e5e896d39fbe
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-25 22:38:15 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,10 +16,11 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+    query_object = str(sys.argv[4])
     cur = db.cursor()
     cur.execute(
         "SELECT * FROM states WHERE name = '{}' ORDER BY ID ASC"
-        .format(str(sys.argv[4]))
+        .format(query_object)
     )
     for name in cur.fetchall():
         print(name)
